cd91d5df84ce54f2c8a066bbe4e6fd22002fadaa
Ryumin <ryumin@lexynaptics.com>
2024-05-20 11:37:59 +0900

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,14 @@ export default function Home() {
     <main>
       <div className="mx-auto mb-12 max-w-lg text-center">
         <div className="m-4">
-          <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl">OpenAI Assistant Starter Kit</h1>
+          <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-5xl">리걸케어 덴탈봇</h1>
           <div className="mb-6 text-normal font-normal text-gray-500">
-            This Starter Kit illustrates how you can create an OpenAI Assistant with ReactJS + NextJS + TypeScript.
-            You can <a href="https://github.com/Superexpert/openai-assistant-starter-kit" target="_new" className="underline text-blue-500">download the source code</a> for 
-            this Starter Kit from GitHub. 
+            병원 운영 중 발생한 법률 이슈에 대해 물어보세요.
           </div>
         </div>
         <OpenAIAssistant 
-          assistantId="asst_gx3Htc0gLVNlpBQKLoefkXZZ"
-          greeting="I am a helpful chat assistant. How can I help you?"
+          assistantId="asst_4WBZX0FS0GlPAHIFUfDe4kzS"
+          greeting="안녕하세요. 리걸케어 덴탈봇입니다. 무엇을 도와드릴까요?"
         />
       </div>
     </main>
